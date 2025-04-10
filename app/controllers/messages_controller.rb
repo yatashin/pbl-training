83c9d662679ccc323742b861bb2ea4820e20d9aa
s-yata <s-yata@creationline.com>
2025-04-10 23:16:11 +0900

    viewの作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,27 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all.order(created_at: :desc)
+    # 全メッセージを作成日時の降順（新しい順）で取得
   end
 
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to messages_path, notice: "メッセージを投稿しました。"
+    else
+      flash.now[:alert] = "メッセージの投稿に失敗しました。"
+      render :new
+    end
+  end
+
+  private
+
+  def message_params
+    # Strong Parameters
+    params.require(:message).permit(:content)
   end
 end
